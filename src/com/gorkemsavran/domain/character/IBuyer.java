b950c97b11a450005e0c9da3bbd701a2eaf036f9
Görkem SAVRAN <gorkem.savran-p@intra.local>
2022-03-14 00:17:46 +0300

    IBuyer created. buy option added

---
+++ src/com/gorkemsavran/domain/character/IBuyer.java
@@ -0,0 +1,7 @@
+package com.gorkemsavran.domain.character;
+
+import com.gorkemsavran.domain.item.Item;
+
+public interface IBuyer {
+    boolean buy(Item item);
+}
--- src/com/gorkemsavran/domain/character/playercharacter/PlayerCharacter.java
+++ src/com/gorkemsavran/domain/character/playercharacter/PlayerCharacter.java
@@ -1,10 +1,28 @@
 package com.gorkemsavran.domain.character.playercharacter;
 
 import com.gorkemsavran.domain.character.Character;
+import com.gorkemsavran.domain.character.IBuyer;
+import com.gorkemsavran.domain.item.Item;
 
-public abstract class PlayerCharacter extends Character {
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class PlayerCharacter extends Character implements IBuyer {
+
+    private List<Item> items;
 
     protected PlayerCharacter(final int id, final int damage, final int health, final int money) {
         super(id, damage, health, money);
+        items = new ArrayList<>();
+    }
+
+    @Override
+    public boolean buy(final Item item) {
+        if (getMoney() < item.getPrice()) {
+            return false;
+        }
+        setMoney(getMoney() - item.getPrice());
+        items.add(item);
+        return true;
     }
 }
